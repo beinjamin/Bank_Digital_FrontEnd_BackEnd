484b0773627eb82b98ada253a780ecba56628d2e
Jaures <jauresbeinajmin@gmail.com>
2022-07-01 20:36:43 +0100

    Mapping Object Relationnel

--- Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/AccountOperation.java
+++ Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/AccountOperation.java
@@ -3,6 +3,10 @@ package com.jaures.demo.entities;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import com.jaures.demo.enums.AccountStatus;
@@ -11,9 +15,13 @@ import com.jaures.demo.enums.OperationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
+
+@Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class AccountOperation {
-	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private Date operationDate;
 	private double amount;
