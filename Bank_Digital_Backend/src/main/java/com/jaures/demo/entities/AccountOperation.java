320c54cb141b0358d9aef3bd71a9c1039954bca1
Jaures <jauresbeinajmin@gmail.com>
2022-07-01 20:10:32 +0100

    Gestion de la class account operation

--- Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/AccountOperation.java
+++ Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/AccountOperation.java
@@ -1,5 +1,12 @@
 package com.jaures.demo.entities;
 
+import java.util.Date;
+
 public class AccountOperation {
+	
+	private Long id;
+	private Date operationDate;
+	private double amount;
+	private OperationType type;
 
 }
