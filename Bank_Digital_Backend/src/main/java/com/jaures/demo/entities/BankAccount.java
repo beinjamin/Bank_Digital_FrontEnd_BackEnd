a7e7304abbdb40b20e2e645706885b264443b404
Jaures <jauresbeinajmin@gmail.com>
2022-07-01 11:45:12 +0100

    Injection lambot

--- Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/BankAccount.java
+++ Bank_Digital_Backend/src/main/java/com/jaures/demo/entities/BankAccount.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 import com.jaures.demo.enums.AccountStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
 	private String id;
 	private double balance;
