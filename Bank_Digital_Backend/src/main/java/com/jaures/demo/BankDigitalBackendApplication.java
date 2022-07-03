e9fd6814746c61ee0921e00613c3f24dbefe8f38
Jaures <jauresbeinajmin@gmail.com>
2022-07-03 23:21:17 +0100

    Mise en place demarrage fausse donne dans la bd

--- Bank_Digital_Backend/src/main/java/com/jaures/demo/BankDigitalBackendApplication.java
+++ Bank_Digital_Backend/src/main/java/com/jaures/demo/BankDigitalBackendApplication.java
@@ -1,13 +1,30 @@
 package com.jaures.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jaures.demo.repositories.AccountOperationRepository;
+import com.jaures.demo.repositories.BankAccountRepository;
+import com.jaures.demo.repositories.CustomerRepository;
 
 @SpringBootApplication
 public class BankDigitalBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankDigitalBackendApplication.class, args);
+	}
+	
+	
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepositor,
+			                BankAccountRepository bankAccountRepositor,
+			                AccountOperationRepository accountOperationRepository) {
+		return args -> {
+			
+		};
+		
 	}
 
 }
