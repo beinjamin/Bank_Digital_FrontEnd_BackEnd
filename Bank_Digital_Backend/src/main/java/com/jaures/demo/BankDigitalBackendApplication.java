d74a1ce92ed5e55455be8543a45bbab236ca94c3
Jaures <jauresbeinajmin@gmail.com>
2022-07-03 23:24:11 +0100

    mise en place des donnée confu dans la bd

--- Bank_Digital_Backend/src/main/java/com/jaures/demo/BankDigitalBackendApplication.java
+++ Bank_Digital_Backend/src/main/java/com/jaures/demo/BankDigitalBackendApplication.java
@@ -1,10 +1,13 @@
 package com.jaures.demo;
 
+import java.util.stream.Stream;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.jaures.demo.entities.Customer;
 import com.jaures.demo.repositories.AccountOperationRepository;
 import com.jaures.demo.repositories.BankAccountRepository;
 import com.jaures.demo.repositories.CustomerRepository;
@@ -22,6 +25,11 @@ public class BankDigitalBackendApplication {
 			                BankAccountRepository bankAccountRepositor,
 			                AccountOperationRepository accountOperationRepository) {
 		return args -> {
+			Stream.of("Jaures","Beinjamin","pascal").forEach(name->{
+				Customer customer=new Customer();
+				customer.setName(name);
+				customer.setEmail(name+"@gmail.com");
+			});
 			
 		};
 		
